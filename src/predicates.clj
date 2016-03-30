b2d3bc2fddd6303d9fc02d7773aadc19b608cbf0
Oskar Bjorkman <theoskarbjorkman@gmail.com>
2016-03-30 11:11:08 +0300

    All checks (42) succeeded...

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,48 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  #(< % n))
 
 (defn equal-to [n]
-  :-)
+  #(== % n))
 
 (defn set->predicate [a-set]
-  :-)
+  #(contains? a-set %))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  #(and (pred1 %) (pred2 %)))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  #(or (pred1 %) (pred2 %)))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+ (every? #(has-award? book %) awards)) 
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [res (filter pred a-seq)]
+    (if (empty? res)
+      nil
+      (pred (first res)))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (apply = (cons true (map pred a-seq))))
 
 (defn prime? [n]
-  :-)
+  (if (== n 2)
+      true
+      (every?
+        #(not (= 0 (rem n %)))
+        (range 2 (inc (Math/ceil (Math/sqrt n)))))))
 ;^^
